75729e45366e209464c55f52f97a122eb60bf47a
diadatp <admin@diadatp.com>
2016-06-14 16:08:49 +0530

    Added script to fetch Micron BFMs

---
+++ sim/models/micron/ddr2/fetch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://www.micron.com/~/media/documents/products/sim-model/dram/dram/39571024mb_ddr2.zip
+unzip 39571024mb_ddr2.zip
+
---
+++ sim/models/micron/ddr3/fetch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://www.micron.com/~/media/documents/products/sim-model/dram/ddr3/ddr3-sdram-verilog-model.zip
+unzip ddr3-sdram-verilog-model.zip
+
