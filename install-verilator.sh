fb116ca83a534832aedb74743867df6637e1917b
diadatp <admin@diadatp.com>
2016-06-25 07:19:37 +0530

    Removed broken Verilator tests

--- install-verilator.sh
+++ install-verilator.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -d "${HOME}/verilator/bin" ]; then
     wget https://github.com/diadatp/verilator/archive/verilator_3_882.tar.gz
     tar -xzvf verilator_3_882.tar.gz && cd verilator-verilator_3_882
-    autoconf && ./configure --prefix="${HOME}/verilator" && make && make test && make install
+    autoconf && ./configure --prefix="${HOME}/verilator" && make && make install
 else
     echo "Using Verilator 3.882 from cached directory."
 fi
